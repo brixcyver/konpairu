6817e0565f085816ad92b6aa7aca8625e29da988
David King Roderos <iamdavidkingroderos@gmail.com>
2023-10-20 20:22:14 +0800

    Created Maui

---
+++ src/Konpairu/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Konpairu
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ src/Konpairu/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Konpairu
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
