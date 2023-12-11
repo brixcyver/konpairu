ef894a0de84dbdfd9355d13fb3cfa94df8fb84b0
David King Roderos <iamdavidkingroderos@gmail.com>
2023-12-11 14:24:35 +0800

    Removed C# keywords

--- src/Konpairu/Model/LexicalAnalyzer.cs
+++ src/Konpairu/Model/LexicalAnalyzer.cs
@@ -106,12 +106,13 @@ public class LexicalAnalyzer
 
     private static bool IsReservedKeyword(string variableName)
     {
-        string[] keywords = { "abstract", "as", "base", "bool", "break", "byte", "case", "catch", "char", "checked", "class", "const", "continue",
-            "decimal", "default", "delegate", "do", "double", "else", "enum", "event", "explicit", "extern", "false", "finally", "fixed",
-            "float", "for", "foreach", "goto", "if", "implicit", "in", "int", "interface", "internal", "is", "lock", "long", "namespace",
-            "new", "null", "object", "operator", "out", "override", "params", "private", "protected", "public", "readonly", "ref",
-            "return", "sbyte", "sealed", "short", "sizeof", "stackalloc", "static", "string", "struct", "switch", "this", "throw",
-            "true", "try", "typeof", "uint", "ulong", "unchecked", "unsafe", "ushort", "using", "var", "virtual", "void", "volatile", "while" };
+        string[] keywords = {
+            "abstract", "assert", "boolean", "break", "byte", "case", "catch", "char", "class", "const",
+            "continue", "default", "do", "double", "else", "enum", "extends", "final", "finally", "float",
+            "for", "if", "implements", "import", "instanceof", "int", "interface", "long", "native", "new",
+            "package", "private", "protected", "public", "return", "short", "static", "strictfp", "super",
+            "switch", "synchronized", "this", "throw", "throws", "transient", "try", "void", "volatile", "while"
+        };
 
         return Array.Exists(keywords, keyword => keyword.Equals(variableName));
     }
